b396c3ff3b43330034bfa497c427f4144397651a
lauraziebarth <lauragzie@gmail.com>
2021-01-28 15:08:34 -0300

    Adiciona logo Alura

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import db from '../db.json';
 import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
@@ -28,6 +29,7 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1> the legend </h1>
